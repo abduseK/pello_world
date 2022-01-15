899b609da42103c7c26955f095a3a50d1c3bd4ba
abdelselam kemal <abdelselamkemal74@gmail.com>
2022-01-15 14:58:20 -0800

    pageView added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon_pro/files/file_one.dart';
 import 'body.dart';
 import 'constants.dart';
 
@@ -12,6 +13,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final controller = PageController(
+    initialPage: 1,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +55,80 @@ class _HomeState extends State<Home> {
         
       ),
 
-      body: const Body(),
+      body: PageView(
+        controller: PageController(),
+        scrollDirection: Axis.vertical,
+        children: const [
+          Page2(),
+          Body(),
+        ]
+      ),
+      
+    );
+  }
+}
+
+
+class Page2 extends StatelessWidget {
+  const Page2({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: RichText(
+          text: const TextSpan(
+            children: <TextSpan>[
+              TextSpan(text: 'Welcome to pello\n\n',
+              style: TextStyle(
+                fontSize: 30.0
+              )
+              ),
+              TextSpan(text: 'This app will give you what u need, ',
+              style: TextStyle(
+                fontFamily: "SassyFrass",
+              ),
+              ),
+              TextSpan(
+                text: 'we have \n\n        - all years modules\n'
+                      '        - project samples\n'
+                      '        - assignment and worksheets\n'
+                      '        - technology books',
+                style: TextStyle(
+                  fontFamily: "SassyFrass",
+                  color: Colors.teal
+                )
+              )
+            ]
+          ),
+        ),
+        elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(70.0),
+            bottomRight: Radius.circular(70.0)
+          )
+        ),
+        toolbarHeight: 300.0,
+        backgroundColor: kPrimaryColor,
+      ),
+
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Scroll',
+            style: TextStyle(
+              color: Colors.teal,
+              fontFamily: "SassyFrass"
+            ),
+            ),
+            Icon(Icons.arrow_downward_rounded,
+            color: Colors.teal,
+            )
+          ],
+        )
+      )
       
     );
   }
